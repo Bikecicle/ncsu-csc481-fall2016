e340423cca5248c8314630e73b1eed82ec9648d3
Griffin <Griffin@10.202.14.127>
2016-09-16 11:48:54 -0400

    progress on part 1

--- hw1/src/part1/DynamicBox.java
+++ hw1/src/part1/DynamicBox.java
@@ -1,14 +1,37 @@
 package part1;
 
+import java.util.List;
+
 public class DynamicBox extends Box {
 
-	public DynamicBox(int posx, int posy, int width, int height, int color) {
+	public final int gravity = 1;
+
+	public float velx;
+	public float vely;
+
+	public DynamicBox(int posx, int posy, int width, int height, int[] color) {
 		super(posx, posy, width, height, color);
+		this.velx = 0;
+		this.vely = 0;
 	}
 
-	public boolean isColliding(Box box) {
-		return this.posx < box.posx + box.width && box.posx < this.posx + this.width
-				&& this.posy < box.posy + box.height && box.posy < this.posy + this.height;
+	public boolean isColliding(List<Box> environment) {
+		for (Box box : environment) {
+			if ((this.posx < box.posx + box.width && box.posx < this.posx + this.width
+					&& this.posy < box.posy + box.height && box.posy < this.posy + this.height) == true )
+				return true;
+		}
+		return false;
+		
+	}
+
+	public void updateYPosition(List<Box> environment) {
+		vely += gravity;
+		posy += vely;
+		if (isColliding(environment)) {
+			posy -= vely;
+			vely = 0;
+		}
 	}
 
 }
--- hw1/src/part1/ProcessingBoxes.java
+++ hw1/src/part1/ProcessingBoxes.java
@@ -1,31 +1,56 @@
 package part1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import processing.core.PApplet;
 
-public class ProcessingBoxes extends PApplet{
+public class ProcessingBoxes extends PApplet {
+
+	public final static int[] black = { 0, 0, 0 };
+	public final static int[] white = { 255, 255, 255 };
+	public final static int[] red = { 255, 0, 0 };
+
+	public List<Box> boxes = new ArrayList<Box>();
+	public DynamicBox player = new DynamicBox(100, 100, 15, 15, red);
 
 	public static void main(String[] args) {
 		PApplet.main("part1.ProcessingBoxes");
 
 	}
-	
+
 	public void settings() {
-		size(300,300);
+		size(700, 700);
 	}
-	
+
 	public void setup() {
-		fill(120,50,240);
+		fill(120, 50, 240);
+		boxes.add(new Box(0, 690, 700, 10, black));
 	}
-	
-	public void draw() { 
-		
+
+	public void draw() {
+		background(255);
+		for (Box box : boxes) {
+			renderBox(box);
+		}
+		player.updateYPosition(boxes);
+		renderBox(player);
 	}
-	
+
+	public void keyPressed() {
+		if (key == CODED) {
+			if (keyCode == UP) {
+				player.vely += -5;
+			}
+		}
+	}
+
+	public void keyReleased() {
+
+	}
+
 	public void renderBox(Box box) {
-		fill(box.color);
+		fill(box.color[0], box.color[1], box.color[2]);
 		rect(box.posx, box.posy, box.width, box.height);
 	}
-	
-	
-
 }
