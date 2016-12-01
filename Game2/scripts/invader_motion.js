4f64b59f8d5052b41a487df0d5ece88b098c7ac5
Griffin <Griffin@10.201.0.247>
2016-12-01 10:52:20 -0500

    done?

--- Game2/scripts/invader_motion.js
+++ Game2/scripts/invader_motion.js
@@ -1,8 +1,9 @@
 function update() {
 	t = time - start;
+	s = 10 + t / 1000000000.0 / 4;
+	movement_component.setVelocityY(-1);
 	px = movement_component.getPosition().getX();
 	py = movement_component.getPosition().getY();
-	s = (initial_y - py) / 4 + 10;
 	vx = movement_component.getVelocityX();
 	vy = movement_component.getVelocityY();
 	if (px <= initial_x) {
@@ -10,8 +11,4 @@ function update() {
 	} else if (px >= 57 + initial_x) {
 		movement_component.setVelocityX(-s);
 	}
-	if (vx != movement_component.getVelocityX()) {
-		movement_component.setTempPositionY(py - 4);
-	}
-	
 }
